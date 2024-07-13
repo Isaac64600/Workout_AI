093511bb7dd2f46ec09d61c006a1c9bf33ac56b6
arthur capo <arthur18.12.01@hotmail.fr>
2024-07-13 13:47:12 +0200

    add new option

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,10 +2,19 @@ document.addEventListener('DOMContentLoaded', function () {
     const exerciseSelect = document.getElementById('exerciseSelect');
     const exerciseVideo = document.getElementById('exerciseVideo');
     const videoSource = exerciseVideo.querySelector('source');
+    const videoFeed = document.getElementById('videoFeed')
 
     exerciseSelect.addEventListener('change', function () {
-        const selectedVideo = exerciseSelect.value;
-        videoSource.setAttribute('src', selectedVideo);
+        const selectedValue = exerciseSelect.value;
+        if (selectedValue === "video_feed") {
+            videoSource.setAttribute('src', '../static/videos/push_ups.mp4');
+            videoFeed.setAttribute('src', 'video_feed');
+            console.log('push up')
+        } else if (selectedValue === "video_feed_pull_up") {
+            videoSource.setAttribute('src', '../static/videos/pull_ups.mp4');
+            videoFeed.setAttribute('src', 'video_feed_pull_up');
+            console.log('pull up')
+        }
         exerciseVideo.load();
     });
 });
